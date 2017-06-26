8e677dce817a13d72d631396955aacb5cf8e3851
Rudolph Hernandez <rudolpharthur@gmail.com>
2017-06-25 23:58:55 -0700

    extract number from input parameter string

--- controllers/convertHandler.js
+++ controllers/convertHandler.js
@@ -2,51 +2,53 @@
 *
 *
 *       Complete the handler logic below
-*       
-*       
+*
+*
 */
 
 function ConvertHandler() {
-  
+
   this.getNum = function(input) {
     var result;
-    
+
+    result = input.match(/\d+([\/.]\d+)?/g);
+
     return result;
   };
-  
+
   this.getUnit = function(input) {
     var result;
-    
+
     return result;
   };
-  
+
   this.getReturnUnit = function(initUnit) {
     var result;
-    
+
     return result;
   };
 
   this.spellOutUnit = function(unit) {
     var result;
-    
+
     return result;
   };
-  
+
   this.convert = function(initNum, initUnit) {
     const galToL = 3.78541;
     const lbsToKg = 0.453592;
     const miToKm = 1.60934;
     var result;
-    
+
     return result;
   };
-  
+
   this.getString = function(initNum, initUnit, returnNum, returnUnit) {
     var result;
-    
+
     return result;
   };
-  
+
 }
 
 module.exports = ConvertHandler;
--- routes/api.js
+++ routes/api.js
@@ -12,7 +12,7 @@ var expect = require('chai').expect;
 var ConvertHandler = require('../controllers/convertHandler.js');
 
 module.exports = function (app) {
-  
+
   var convertHandler = new ConvertHandler();
 
   app.route('/api/convert')
@@ -23,8 +23,8 @@ module.exports = function (app) {
       var returnNum = convertHandler.convert(initNum, initUnit);
       var returnUnit = convertHandler.getReturnUnit(initUnit);
       var toString = convertHandler.getString(initNum, initUnit, returnNum, returnUnit);
-      
-      //res.json
+
+      res.json({ initNum: initNum });
     });
-    
+
 };
